55d037fb8e9232362c3076689c8cbcc250950bb2
jordi <jordi@jordi-Mint>
2015-05-20 13:22:08 +0200

    Traducció al català de 'permissioninfo'

---
+++ rw_html/ewikiform/lib/plugins/permissioninfo/lang/ca/lang.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * english language file for permissioninfo plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Gabriel Birke <gb@birke-software.de>
+ */
+ 
+// settings must be present and set appropriately for the language
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+ 
+// for admin plugins, the menu prompt to be displayed in the admin menu
+// if set here, the plugin doesn't need to override the getMenuText() method
+$lang['menu'] = 'Informació de permisos'; 
+$lang['users'] = 'Usuaris';
+$lang['permissions'] = 'Permisos';
+
+$lang['pi_resource'] = 'Namespace / Pàgina';
+$lang['pi_permissionfor'] = "Informació de permisos per a %s";
+$lang['pi_to_overview'] = "Torna enrere";
+$lang['pi_no_permissions_found'] = "No s'ha trobat permisos.";
+
+// This was copied form the lang file of the acl plugin
+$lang['acl_perm1']  = 'Lectura';
+$lang['acl_perm2']  = 'Edició';
+$lang['acl_perm4']  = 'Creació';
+$lang['acl_perm8']  = 'Pujada';
+$lang['acl_perm16'] = 'Esborrar';
+
+
